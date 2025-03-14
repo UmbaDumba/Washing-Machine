0cc71bd46dde31994e3175f3d489c0b6826518a5
UmbaDumba <dkdi3104@gmail.com>
2025-03-14 14:00:09 +0900

    init proj

---
+++ Washing_machine/Washing_machine/main.c
@@ -0,0 +1,18 @@
+/*
+ * Washing_machine.c
+ *
+ * Created: 2025-03-14 오후 1:59:46
+ * Author : microsoft
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
